6397938a811e4e4c6132dcba8d331a317b666a41
thewritingstew <richard.o.stewart@gmail.com>
2016-11-20 10:26:27 -0700

    adding files from default python project skeleton

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'description': 'My Project',
+        'author': 'Richard Stewart',
+        'url': 'URL to get at it.',
+        'download_url': 'where to download it.',
+        'author_email': 'richard.o.stewart@gmail.com',
+        'version': '0.1',
+        'install_requires': ['nose'],
+        'packages': ['NAME'],
+        'scripts': [],
+        'name': 'projectname'
+    }
+
+setup(**config)
