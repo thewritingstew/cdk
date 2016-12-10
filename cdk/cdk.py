0346019070d0995ea378f68e82fb1ea1a82ae039
thewritingstew <richard.o.stewart@gmail.com>
2016-12-09 22:35:25 -0700

    working on iss #6, also additional capability toward adding C and D's games to the program

--- cdk/cdk.py
+++ cdk/cdk.py
@@ -7,8 +7,11 @@ from context import Context
 def create_default_actions():
     # for items in a file, import the default games and create contexts
     # for now these items will be hard-coded
-    game_one = Context(name="Carson's game")
-    game_two = Context(name="Davis's game")
+    game_one = Context(name="Carson's game",
+        init_display="Welcome to Carson's game. Enter 'start' to proceed...")
+    
+    game_two = Context(name="Davis's game",
+        init_display="Welcome to Davis's game. Enter 'start' to proceed...")
 
     # TODO: add more content to the default games, such as additional contexts
     # and the like. May want to pull the additional contexts out of a file,
@@ -17,8 +20,8 @@ def create_default_actions():
     # other programming tasks. 
     
     default_menu_items = {
-        0:game_one,
-        1:game_two
+        '0':(game_one.context_name, game_one),
+        '1':(game_two.context_name, game_two)
         }
     return default_menu_items
 
@@ -39,6 +42,11 @@ def main():
     # print("After: ", main_menu.action_paths)  # DEBUG
     
     while True:
+        # TODO: if current_context.skip_to == empty, then do what we have already 
+        # defined below. Otherwise, set current_context to something else and 
+        # return to top of while loop. This will be accomplished with the if-
+        # else, but we could also use a 'continue' to jump back to the top of 
+        # the loop and try it again. 
         current_context.display()  # display current context's display
         current_context.get_input()  # process the user's input
         current_context = current_context.next_context()  # get next context
--- cdk/context.py
+++ cdk/context.py
@@ -5,7 +5,7 @@ every context that displays things and takes input from the user.
 
 class Context(object):
     
-    def __init__(self, name=""):
+    def __init__(self, name="", init_display=""):
         """
         Initializes a Context object. 
         :param name: string 
@@ -13,14 +13,17 @@ class Context(object):
         """
         self.context_name = name  # name of the context
         self.action_paths = {
-            'quit':False,
-            'help':False
+            'quit':("Quit the game", False),
+            'help':("Get help", ("Type 'hint' to get a list of available actions (if any), "
+                "'q' to return to main menu, "
+                "or 'quit' to exit the game entirely."
+                ))
             }  # list of navigation contexts, help, quit
         self.hints_on = True  # boolean telling whether hints are allowed
-        self._goto_context = self
+        self._goto_context = self  # goto points to self for now
+        self._initial_display = init_display  # set initial display
 
-
-    def execute_choice(self):
+    def execute_choice(self):  # TODO: delete this? Not sure what it does.
         pass
 
 
@@ -29,48 +32,81 @@ class Context(object):
             self.action_paths[key] = context_list[key]
 
 
-    def get_hints(self):
+    def get_hints(self):  # TODO: update to format dictionary for printing
         if self.hints_on:
-            for i, j in enumerate(self.action_paths):  # unpack tuple to i, j
+            print(
+                "\n",
+                "%10s" % "COMMAND",  # column 1 header
+                " " * 4,  # print four spaces
+                "ACTION"  # column 2 header
+            )
+            for key in sorted(self.action_paths):  # iterate through action_paths dict
                 print(
-                      "\t",  # print a tab to move things over
-                      "%4d" % i,  # print the index, four characters wide
+                      "%10s" % key,  # print the key, 10 characters wide
                       " " * 4,  # print four spaces
-                      j  # print the list item
+                      self.action_paths[key][0]  # print the dict value
                       )
-            print("Please select an item from the list above.")
+            print("\nPlease type a command from the list above.")
         else:
-            print("Sorry, no hints available from here. Try typing '::help'")
+            print("Sorry, no hints available from here. Try typing 'help'",
+                " for help or 'q' to return to the main menu.")
+
+
+    # TODO: Update this to print the Context object
+    # def __repr__(self):
+        # """
+        # Create a basic functionality for printing nicely
+        # :return: a string
+        # """
+        # return ""  
 
 
     # current_context.display()  # display current context's display
-    def display(self):
+    def display(self, prior_context=""):
         # TODO: pretty up this print function
-        print(self.action_paths)
+        print("~"*70)
+        print("~"*70)
+        if self._initial_display != "":  # displays initial display if not blank
+            print(self._initial_display)
+        else:
+            self.get_hints()  # use get_hints for printing
 
         
         
     # current_context.get_input()  # process the user's input
     def get_input(self):
         # process the input for the current context
-        usr_input = input("What would you like to do? ==> ")  # get input
-        print("User input is: {}".format(usr_input))
+        usr_input = input("\nWhat would you like to do? ==> ")  # get input
+        # print("User input is: {}".format(usr_input))  # DEBUG
         self._process_input(usr_input)  # process the input
 
         
     # current_context = current_context.next_context()  # get next context
     def next_context(self):
-        # TODO: figure out what the next context is, and return it
+        """
+        Return the next context to go to 
+        :return: context
+        """
         return(self._goto_context)
         
 
     def _process_input(self, usr_in):
         command = usr_in.split()[0]
         
-        if command == 'quit' or command == 'q':  # could use if 'qu' in command
-            self._goto_context = self.action_paths['quit']
+        ### if dict keys are not integers, then this try isn't needed
+        # try:  # change input from string to int if applicable
+        #   # command = int(command)
+        # except ValueError:
+        #   # pass
+        ###
+        
+        if command == 'q':  # alternate of 'quit'
+            self._goto_context = self.action_paths['quit'][1]
+        elif command == 'h' or command == 'help':  # alternate of 'help'
+            print(self.action_paths['help'][1])  # TODO: iss #6 temp fix
+            # self._goto_context = self.action_paths['help'][1]
         elif command in self.action_paths:
-            self._goto_context = self.action_paths[command]
+            self._goto_context = self.action_paths[command][1]
         else:
             self._goto_context = self
             
